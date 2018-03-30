99b1c41618d06715ee5e7a767ec4215cf2e162c1
dezoul <aussieme@gmail.com>
2018-03-29 21:56:27 -0700

    deck flip

--- js/main.js
+++ js/main.js
@@ -3,16 +3,19 @@ $(document).ready(function() {
   $('.wall').masonry({
     // options
     itemSelector: '.brick',
+    columnWidth: 0,
     gutter: 20,
     originLeft: false
   });
 window.onload = function() {
 
-  $('.wall').masonry({
+  $('.wall').delay(200).masonry({
     // options
     itemSelector: '.brick',
     gutter: 20,
-    originLeft: false
+    originLeft: false,
+    columnWidth: 300,
+    stagger: 30
   });
 
 };
